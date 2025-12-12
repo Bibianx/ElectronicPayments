5a23f68660778536384e5bdc2320b228d3ee596f
Bibianx <ladytorres@ierv.edu.co>
2025-12-12 17:16:00 -0500

    feat(Entities): Se crean modelos para llevar auditoria de los pagos por PSE realizados por el proveedor ZonaPagos

---
+++ Domain/Entities/PSE/ZonaPagos/HISTORIALZP.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Entities.PSE.ZonaPagos
+{
+    [Index(nameof(id_pago))]
+    public class HISTORIALZP
+    {
+        [Key]
+        public Guid id_historial { get; set; } = Guid.NewGuid();
+
+        [MaxLength(50)]
+        public string cod_estado_ini { get; set; }
+
+        [MaxLength(50)]
+        public string descrip_estado_ini { get; set; }
+
+        [MaxLength(50)]
+        public string cod_estado_fin { get; set; }
+
+        [MaxLength(50)]
+        public string descrip_estado_fin { get; set; }
+
+        [MaxLength(255)]
+        public string detalle_pago { get; set; }
+
+        [MaxLength(50)]
+        public string origen_cambio { get; set; }
+
+        [Column(TypeName = "jsonb")]
+        public string json_respuesta { get; set; }
+        public DateOnly? fecha_ini { get; set; }
+        public DateOnly? fecha_fin { get; set; }
+        public TimeOnly? hora_ini { get; set; }
+        public TimeOnly? hora_fin { get; set; }
+        public bool? contabilizado_cobol_ok { get; set; }
+        public bool? ticket_cobol_ok { get; set; }
+        public string estado_cobol_ini { get; set; }
+        public string detalle_cobol_ini { get; set; }
+        public string estado_cobol_fin { get; set; }
+        public string detalle_cobol_fin { get; set; }
+        public Guid id_pago { get; set; }
+
+        [ForeignKey("id_pago")]
+        public INTENTOSZP intentos_zp { get; set; }
+    }
+}
---
+++ Domain/Entities/PSE/ZonaPagos/INTENTOSZP.cs
@@ -0,0 +1,78 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Entities.PSE.ZonaPagos
+{
+    [Index(nameof(str_id_pago))]
+    public class INTENTOSZP
+    {
+        [Key]
+        public Guid id_pago { get; set; } = Guid.NewGuid();
+
+        [Required, Column(TypeName = "decimal(16, 2)")]
+        public decimal flt_total_con_iva { get; set; }
+
+        [Column(TypeName = "decimal(16, 2)")]
+        public decimal flt_valor_iva { get; set; }
+
+        [Required, MaxLength(30)]
+        public string str_id_pago { get; set; }
+
+        [Required, MaxLength(70)]
+        public string str_descripcion_pago { get; set; }
+
+        [MaxLength(70)]
+        public string str_email { get; set; }
+
+        [MaxLength(30)]
+        public string str_id_cliente { get; set; }
+
+        [MaxLength(5)]
+        public string str_tipo_id { get; set; }
+
+        [MaxLength(50)]
+        public string str_nombre_cliente { get; set; }
+
+        [MaxLength(50)]
+        public string str_apellido_cliente { get; set; }
+
+        [MaxLength(50)]
+        public string str_telefono_cliente { get; set; }
+
+        [MaxLength(70)]
+        public string str_opcional1 { get; set; }
+
+        [MaxLength(70)]
+        public string str_opcional2 { get; set; }
+
+        [MaxLength(70)]
+        public string str_opcional3 { get; set; }
+
+        [MaxLength(70)]
+        public string str_opcional4 { get; set; }
+
+        [MaxLength(70)]
+        public string str_opcional5 { get; set; }
+
+        [Required]
+        public int int_id_comercio { get; set; }
+
+        [Required, MaxLength(40)]
+        public string str_usuario { get; set; }
+
+        [Required, MaxLength(50)]
+        public string str_clave { get; set; }
+
+        [Required]
+        public int int_modalidad { get; set; }
+
+        [Required]
+        public int numero_ticket_cobol { get; set; }
+
+        [MaxLength(2)]
+        public string estado_intento { get; set; }
+        public DateOnly? fecha_intento { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public TimeOnly? hora_intento { get; set; } = TimeOnly.FromDateTime(DateTime.Now);
+    }
+}
