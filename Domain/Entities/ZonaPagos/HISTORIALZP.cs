72ea88c4353fe29eeb109412be275d521328ffba
Bibianx <ladytorres@ierv.edu.co>
2025-12-15 09:12:10 -0500

    fix(Entities): Se mueven archivos tras eliminar una carpeta disfuncional

--- Domain/Entities/ZonaPagos/HISTORIALZP.cs
+++ Domain/Entities/ZonaPagos/HISTORIALZP.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace Domain.Entities.PSE.ZonaPagos
+namespace Domain.Entities.ZonaPagos
 {
     [Index(nameof(id_pago))]
     public class HISTORIALZP
--- Domain/Entities/ZonaPagos/INTENTOSZP.cs
+++ Domain/Entities/ZonaPagos/INTENTOSZP.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace Domain.Entities.PSE.ZonaPagos
+namespace Domain.Entities.ZonaPagos
 {
     [Index(nameof(str_id_pago))]
     public class INTENTOSZP
