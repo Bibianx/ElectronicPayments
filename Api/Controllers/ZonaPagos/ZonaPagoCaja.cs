646eb519499951e5293032e07c23979a7e2b88f5
Bibianx <ladytorres@ierv.edu.co>
2025-12-15 09:28:02 -0500

    feat(Controllers): Se crea controlador para los endpoints del servicio de caja con zona pagos

---
+++ Api/Controllers/ZonaPagos/ZonaPagoCaja.cs
@@ -0,0 +1,29 @@
+using Aplication.DTOs.ZonaPagos;
+using Aplication.Services.Recaudos.ZonaPagoCaja;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.ZonaPagos
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]    
+    [AllowAnonymous]
+    public class ZonaPagosCajaController(IZonaPagoCaja ZonaPagoCajaServices) : ControllerBase
+    {
+        private readonly IZonaPagoCaja _servicesZonaPagoCaja = ZonaPagoCajaServices;
+
+        [HttpPost("consultar-pago")]
+        public async Task<ActionResult<ConsultaPagoDto>> ConsultarFacturas(ConsultaPagoParams _)
+        {
+            return await _servicesZonaPagoCaja.ConsultarFactura(_);
+        }
+
+        [HttpPost("asentar-pago")]
+        public async Task<ActionResult<AsientoPagoDto>> AsentarPago(AsientoPagoParams _)
+        {
+            return await _servicesZonaPagoCaja.AsentarPago(_);
+        }
+        
+    }
+}
