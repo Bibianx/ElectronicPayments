34a7e11537451584b7fe4c134161b7a33ea3db81
Bibianx <ladytorres@ierv.edu.co>
2025-12-16 16:40:25 -0500

    feat(services): se implementa servicio de integración Dominus usando HttpClient y DTOs definidos

---
+++ Application/Services/Dominus/DominusServices.cs
@@ -0,0 +1,146 @@
+using System.Text.Json;
+using Aplication.DTOs.Dominus;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace Aplication.Services.Dominus
+{
+    public interface IDominus
+    {
+        Task<ServiceResponse<ResponseToken>> GenerarToken(RequestToken request);
+        Task<ServiceResponse<ResponseListadoConsolidados>> ConsultarListadoConsolidados(RequestListadoConsolidados request);
+        Task<ServiceResponse<ConsultaVentasConsolidadoResponse>> ConsultarVentasConsolidado(RequestConsultaVentasConsolidado request);
+    }
+
+    public class DominusServices(IHttpClientFactory httpClientFactory) : IDominus
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+        private static string token_cache;
+        public async Task<ServiceResponse<ResponseToken>> GenerarToken(RequestToken request)
+        {
+            var response = new ServiceResponse<ResponseToken>();
+            try
+            {
+                var cliente = _httpClientFactory.CreateClient("Dominus");
+
+                var consulta_api_dominus = await cliente.PostAsJsonAsync("oauth/v2/token", request);
+                if (!consulta_api_dominus.IsSuccessStatusCode)
+                {
+                    response.success = false;
+                    response.message = "Error al comunicarse con el servicio Dominus";
+                    return response;
+                }
+
+                var contenido_respuesta = await consulta_api_dominus.Content.ReadFromJsonAsync<ResponseToken>();
+                token_cache = contenido_respuesta.access_token;
+                
+                response.success = true;
+                response.message = "Token generado exitosamente";
+                response.data = new ResponseToken
+                {
+                    access_token = contenido_respuesta.access_token,
+                    expires_in = contenido_respuesta.expires_in,
+                    token_type = contenido_respuesta.token_type
+                };
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.error = $"Excepción al comunicarse con el servicio Dominus {ex.Message}";
+                return response;
+                
+            }
+            return response;
+        }
+
+        public async Task<ServiceResponse<ResponseListadoConsolidados>> ConsultarListadoConsolidados(RequestListadoConsolidados request)
+        {
+            var response = new ServiceResponse<ResponseListadoConsolidados>();
+            try
+            {
+                var cliente = _httpClientFactory.CreateClient("Dominus");
+                cliente.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token_cache);
+
+                var queryParams = new Dictionary<string, string>
+                {
+                    ["branch_id"] = request.branch_id.ToString(),
+                    ["start_date"] = request.start_date.ToString(),
+                    ["final_date"] = request.final_date.ToString()
+                };
+
+                var url = QueryHelpers.AddQueryString(
+                    "integrations/administrative/dominus/consolidatedlist",
+                    queryParams
+                );
+
+                var httpResponse = await cliente.GetAsync(url);
+                var content_response = await httpResponse.Content.ReadAsStringAsync();
+
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    response.success = false;
+                    response.message = "Error al comunicarse con el servicio Dominus";
+                    response.error = content_response;
+                    return response;
+                }
+
+                var contenido = JsonSerializer.Deserialize<ResponseListadoConsolidados>(content_response);
+
+                response.success = true;
+                response.message = "Consulta exitosa";
+                response.data = contenido;
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.error = $"Excepción al comunicarse con Dominus: {ex.Message}";
+            }
+
+            return response;
+        }
+
+      
+        public async Task<ServiceResponse<ConsultaVentasConsolidadoResponse>> ConsultarVentasConsolidado(RequestConsultaVentasConsolidado request)
+        {
+            var response = new ServiceResponse<ConsultaVentasConsolidadoResponse>();
+            try
+            {
+                var cliente = _httpClientFactory.CreateClient("Dominus");
+                cliente.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token_cache);
+
+                var queryParams = new Dictionary<string, string>
+                {
+                    ["consolidated_id"] = request.consolidated_id,
+                };
+
+                var url = QueryHelpers.AddQueryString(
+                    "integrations/administrative/dominus/consolidated",
+                    queryParams
+                );
+
+                var httpResponse = await cliente.GetAsync(url);
+                var content_response = await httpResponse.Content.ReadAsStringAsync();
+
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    response.success = false;
+                    response.message = "Error al comunicarse con el servicio Dominus";
+                    response.error = content_response;
+                    return response;
+                }
+                var contenido = JsonSerializer.Deserialize<ConsultaVentasConsolidadoResponse>(content_response);
+                response.success = true;
+                response.message = "Consulta exitosa";
+                response.data = contenido;
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.error = $"Excepción al comunicarse con el servicio Dominus {ex.Message}";
+                return response;
+                
+            }
+            return response;
+        }
+    }
+    
+}
