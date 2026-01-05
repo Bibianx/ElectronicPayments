cc37bcfc82a34eabb5b78ba873718da073a93fd6
Bibianx <ladytorres@ierv.edu.co>
2026-01-05 10:20:01 -0500

    feat(CAT204GDto): Se agrega nueva estructura dto para realizar consumo de dll de contabilizacion de factura de predial

---
+++ Application/DTOs/Predial/CAT204GDto.cs
@@ -0,0 +1,11 @@
+namespace Aplication.DTOs.Predial
+{
+    public class CAT204GDto
+    {
+        public string usuario { get; set; }
+        public string sesion { get; set; }
+        public string nro_cat { get; set; }
+        public string ano_fin { get; set; }
+        public decimal vlr_fac { get; set; }
+    }
+}
